fe94d39d85557fd0972a5f6f3cc60e3e7a893aa4
kgmx <rika@wantedly.com>
2013-10-30 13:02:53 +0900

    Create nu-mag

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NuMag::Application.config.secret_key_base = '8101f976b68ce4b26781ec63b488759975c9f9aae9e5cfd301b22bd4bbad68b9d1c2013a6c941cd1b72b4d38f2d770f03215ad6367f6a781b6adce663c27d745'
